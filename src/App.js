c3130f5fbcfdd12a8836836f9fe9f84cd75dc3bb
nafisholeh <rachmadnafisholeh@gmail.com>
2024-08-17 21:53:00 +0700

    feat(route): setup route for view feed and login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { UserAuthContextProvider } from "./contexts/authContext";
+import Login from "./components/Login";
+import DogBreeds from "./components/DogBreeds";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserAuthContextProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/feed" element={<DogBreeds />} />
+        </Routes>
+      </Router>
+    </UserAuthContextProvider>
   );
 }
 
---
+++ src/components/DogBreeds.jsx
@@ -0,0 +1,5 @@
+const DogBreeds = () => {
+  return <></>;
+};
+
+export default DogBreeds;
